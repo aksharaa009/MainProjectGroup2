b8ff04b99f45696d37362631a7e811a213083529
unknown <aksharaa009@gmail.com>
2024-05-26 22:22:19 +0530

    added quit,constants,pages

---
+++ src/test/java/StepDefinitions/TestBase.java
@@ -0,0 +1,24 @@
+package StepDefinitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class TestBase {
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.get("http://64.227.132.109/");
+	}
+	
+	@AfterTest
+	public void tearDown() 
+	{
+		driver.quit();
+		}
+	
+
+}
