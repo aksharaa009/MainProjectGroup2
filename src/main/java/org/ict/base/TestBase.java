23566ab96e30687075093541153b58855768f847
unknown <aksharaa009@gmail.com>
2024-05-28 12:48:48 +0530

    changed directory structure

--- src/main/java/org/ict/base/TestBase.java
+++ src/main/java/org/ict/base/TestBase.java
@@ -1,4 +1,6 @@
-package StepDefinitions;
+package org.ict.base;
+
+import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,14 +12,18 @@ public class TestBase {
 	
 	@BeforeTest
 	public void setUp() {
-		driver = new ChromeDriver();
+		driver = new ChromeDriver();	
 		driver.get("http://64.227.132.109/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 	
 	@AfterTest
 	public void tearDown() 
 	{
-	driver.quit();
+		if (driver != null) {
+            driver.quit();
+        }
 		}
 	
 
